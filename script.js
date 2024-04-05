35e441aa09d77ff4bdd024f0ea9ec1bba7685ce0
Rishabh Singh <52191143+rishabh0085@users.noreply.github.com>
2024-04-06 00:07:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+    var select = document.getElementById("colorSelect");
+    var button = document.querySelector('input[type="button"]');
+    
+    button.addEventListener("click", function() {
+        var selectedIndex = select.selectedIndex;
+        if (selectedIndex !== -1) {
+            select.remove(selectedIndex);
+        }
+    });
+});
